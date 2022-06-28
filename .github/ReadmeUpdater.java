6b5d3fe9651b73ef406b49a0882ef97a97f7af02
ls-coder0813 <108300582+ls-coder0813@users.noreply.github.com>
2022-06-28 21:54:26 +0800

    Update ReadmeUpdater.java

--- .github/ReadmeUpdater.java
+++ .github/ReadmeUpdater.java
@@ -1,3 +1,4 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,14 +11,18 @@ public class ReadmeUpdater {
     private static final String MAVEN_VERSION = "<version>.*</version>";
     private static final String GRADLE_VERSION = "implementation 'com\\.featureprobe:server-sdk-java:.*'";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         String releaseVersion = args[0];
 
         String new_content = new String(Files.readAllBytes(README_PATH))
                 .replaceAll(MAVEN_VERSION, String.format("<version>%s</version>", releaseVersion))
                 .replaceAll(GRADLE_VERSION, String.format("implementation 'com\\.featureprobe:server-sdk-java:%s", releaseVersion));
 
-        System.out.println(new_content);
+        try (FileWriter writer = new FileWriter(README_PATH.toString(), false)) {
+            writer.write(new_content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
